5206a54142d1e62577fdd6e3ad472a3a0f197573
neokage <66198267+mikecognata@users.noreply.github.com>
2022-08-08 21:10:56 -0400

    Authentication / Tokens / DB && Middlware Practice

---
+++ config/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const connectDB = async() => {
+    await mongoose.connect(process.env.MONGO_URI).then(() => {
+        console.log("ZionDB is online");
+     });
+
+};
+
+module.exports = connectDB;
---
+++ controllers/auth.js
@@ -0,0 +1,73 @@
+const User = require('../models/User');
+
+
+exports.register = async (req, res, next) => {
+    const {username, email, password} = req.body;
+
+    try {
+        const user = await User.create({
+            username, 
+            email, 
+            password,
+        });
+
+        res.status(201).json({
+            success: true,
+            user,
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            error: error.message,
+        });
+    }
+};
+
+exports.login = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    if(!email || !password) {
+        res.status(400).json({
+            success: false,
+            error  : "Please provide a valid email or password"
+        });
+    }
+
+    try {
+        const user = await User.findOne({ email }).select("+password");
+
+        if(!user) {
+            res.status(404).json({
+                success: false,
+                error  : "Invalid Credentials"
+            });
+        }
+
+        const isMatch = await user.matchPasswords(password);
+
+        if(!isMatch){
+            res.status(404).json({
+                success: false,
+                error  : "Invalid Credentials"});
+        }
+
+        res.status(200).json({
+            success: true,
+            token  : "lafghaliufgh",
+        });
+
+    }   catch (error) {
+        res.status(500).json({
+            success: false,
+            error: error.message,
+        });
+    }
+};
+
+exports.forgotpassword = (req, res, next) => {
+    res.send("Forgot Password Route");
+};
+
+exports.resetpassword = (req, res, next) => {
+    res.send("Reset Password Route");
+};
